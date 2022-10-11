680e4ed52c064c52d8df3e58d9d96cb2e47ca62c
EndeavoringOrb <aaronbelikoff@gmail.com>
2022-10-11 09:22:34 -0700

    1st commit

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 
 num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
+num3 = []
+smaller = len(num2)
+if len(num1) < len(num2):
+    smaller = len(num1)
+
+for i in range(smaller):
+    num3.append(num1[0])
+    num1.pop(0)
+    num3.append(num2[0])
+    num2.pop(0)
+num3.append(num1 + num2)
 
 # ******************************
 # Make your Code
